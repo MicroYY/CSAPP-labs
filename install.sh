0d688f2aaaf7d531fd226134a8073d785684fee5
MicroYY <jianhuyy@hotmail.com>
2023-03-14 23:25:13 +0800

    first commit and datalab ready

---
+++ install.sh
@@ -0,0 +1,7 @@
+cd datalab
+wget http://csapp.cs.cmu.edu/3e/README-datalab
+wget http://csapp.cs.cmu.edu/3e/datalab.pdf
+wget http://csapp.cs.cmu.edu/3e/datalab-handout.tar
+tar xvf datalab-handout.tar
+cd ..
+
