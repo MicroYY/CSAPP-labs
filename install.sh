d052aa176668567b17c283460e3baee0b9dad4f3
MicroYY <jianhuyy@hotmail.com>
2023-03-26 16:56:39 +0800

    bomblab ready

--- install.sh
+++ install.sh
@@ -5,3 +5,10 @@ wget http://csapp.cs.cmu.edu/3e/datalab-handout.tar
 tar xvf datalab-handout.tar
 cd ..
 
+cd bomblab
+#wget http://csapp.cs.cmu.edu/im/labs/bomblab.tar
+wget http://csapp.cs.cmu.edu/3e/README-bomblab
+wget http://csapp.cs.cmu.edu/3e/bomblab.pdf
+wget http://csapp.cs.cmu.edu/3e/bomb.tar
+tar xvf bomb.tar
+cd ..
