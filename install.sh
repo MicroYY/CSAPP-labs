d364e95ed9a39893449ece7bf685c50431dedeb6
MicroYY <jianhuyy@hotmail.com>
2023-04-09 23:30:19 +0800

    attacklab ready

--- install.sh
+++ install.sh
@@ -12,3 +12,12 @@ wget http://csapp.cs.cmu.edu/3e/bomblab.pdf
 wget http://csapp.cs.cmu.edu/3e/bomb.tar
 tar xvf bomb.tar
 cd ..
+
+
+cd attacklab
+#wget http://csapp.cs.cmu.edu/im/labs/attacklab.tar
+wget http://csapp.cs.cmu.edu/3e/README-attacklab
+wget http://csapp.cs.cmu.edu/3e/attacklab.pdf
+wget http://csapp.cs.cmu.edu/3e/target1.tar
+tar xvf target1.tar
+cd ..
