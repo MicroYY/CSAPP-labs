2742a70b00e17a6eea223671149e6c4f295ea285
MicroYY <jianhuyy@hotmail.com>
2023-06-27 00:07:03 +0800

    cachelab done

--- cachelab/cachelab-handout/csim.c
+++ cachelab/cachelab-handout/csim.c
@@ -1,7 +1,260 @@
+#include <getopt.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
 #include "cachelab.h"
 
-int main()
+void printHelp()
 {
-    printSummary(0, 0, 0);
+    printf("Usage: ./csim-ref [-hv] -s <num> -E <num> -b <num> -t <file>\n"
+            "Options:\n"
+            "  -h         Print this help message.\n"
+            "  -v         Optional verbose flag.\n"
+            "  -s <num>   Number of set index bits.\n"
+            "  -E <num>   Number of lines per set.\n"
+            "  -b <num>   Number of block offset bits.\n"
+            "  -t <file>  Trace file.\n\n"
+            "Examples:\n"
+            "  linux>  ./csim-ref -s 4 -E 1 -b 4 -t traces/yi.trace\n"
+            "  linux>  ./csim-ref -v -s 8 -E 2 -b 4 -t traces/yi.trace\n");
+}
+
+int numMiss = 0, numHit = 0, numEvict = 0;
+
+int printUsage = 0;
+int printTrace = 0;
+char tracefile[256];
+
+typedef struct
+{
+    int valid;
+    int tag;
+    unsigned timeStamp;
+} CacheLine;
+
+typedef struct
+{
+    CacheLine* cacheLine;
+    int maxStampIdx;
+} CacheSet;
+
+typedef struct
+{
+    CacheSet* cacheSet;
+    int S;
+    int E;
+    int B;
+} Cache;
+
+Cache* InitCache(int S, int E, int B)
+{
+    Cache* cache    = (Cache*)malloc(sizeof(Cache));
+    cache->S        = S;
+    cache->E        = E;
+    cache->B        = B;
+    cache->cacheSet = (CacheSet*)malloc(sizeof(CacheSet) * S);
+    for (size_t i = 0; i < S; i++)
+    {
+        cache->cacheSet[i].cacheLine = (CacheLine*)malloc(sizeof(CacheLine) * E);
+        cache->cacheSet[i].maxStampIdx       = 0;
+        for (size_t j = 0; j < E; j++)
+        {
+            cache->cacheSet[i].cacheLine[j].tag       = -1;
+            cache->cacheSet[i].cacheLine[j].timeStamp = 0;
+            cache->cacheSet[i].cacheLine[j].valid     = 0;
+        }
+        
+    }
+    return cache;
+}
+
+void DestroyCache(Cache* cache)
+{
+    for (size_t i = 0; i < cache->S; i++)
+    {
+        free(cache->cacheSet[i].cacheLine);
+    }
+    free(cache->cacheSet);
+    free(cache);
+    
+}
+
+int GetLineIdx(Cache* cache, unsigned CT, unsigned CI, unsigned CO)
+{
+    for (int i = 0; i < cache->E; i++)
+    {
+        if (cache->cacheSet[CI].cacheLine[i].valid && 
+            cache->cacheSet[CI].cacheLine[i].tag == CT)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+void UpdateCacheSet(CacheSet* cacheSet, int cacheLineIdx, unsigned CT)
+{
+    cacheSet->cacheLine[cacheLineIdx].valid = 1;
+    cacheSet->cacheLine[cacheLineIdx].tag   = CT;
+    cacheSet->cacheLine[cacheLineIdx].timeStamp = cacheSet->cacheLine[cacheSet->maxStampIdx].timeStamp + 1;
+    cacheSet->maxStampIdx = cacheLineIdx;
+}
+
+int FindEmptyLine(CacheSet* cacheSet, int E)
+{
+    for (size_t i = 0; i < E; i++)
+    {
+        if(cacheSet->cacheLine[i].valid == 0)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int Evict(CacheSet* cacheSet, int E)
+{
+    unsigned min = cacheSet->cacheLine[0].timeStamp;
+    unsigned max = min;
+    int minIdx = 0; int maxIdx = 0;
+    for (size_t i = 1; i < E; i++)
+    {
+        if (cacheSet->cacheLine[i].timeStamp < min)
+        {
+            min = cacheSet->cacheLine[i].timeStamp;
+            minIdx = i;
+        }
+        if (cacheSet->cacheLine[i].timeStamp > max)
+        {
+            max = cacheSet->cacheLine[i].timeStamp;
+            maxIdx = i;
+        }
+    }
+    cacheSet->cacheLine[minIdx].timeStamp = cacheSet->cacheLine[maxIdx].timeStamp;
+
+    return minIdx;
+}
+
+void UpdateCache(Cache* cache, unsigned CT, unsigned CI, unsigned CO)
+{
+    int lineIdx = GetLineIdx(cache, CT, CI, CO);
+    if (lineIdx == -1)
+    {
+        if (printTrace) printf(" miss");
+        numMiss++;
+        lineIdx = FindEmptyLine(&(cache->cacheSet[CI]), cache->E);
+        if(lineIdx == -1)
+        {
+            if (printTrace) printf(" eviction");
+            numEvict++;
+            lineIdx = Evict(&(cache->cacheSet[CI]), cache->E);
+        }
+        UpdateCacheSet(&cache->cacheSet[CI], lineIdx, CT);
+    }
+    else
+    {
+        if (printTrace) printf(" hit");
+        numHit++;
+        UpdateCacheSet(&cache->cacheSet[CI], lineIdx, CT);
+    }
+}
+
+#define digits 64
+
+int main(int argc, char** argv)
+{
+    if (argc == 1)
+    {
+        printHelp();
+        return 0;
+    }
+    
+    int s = 0, E = 0, b = 0;
+    int S = 0, B = 0;
+
+    int arg;
+    while ((arg = getopt(argc, argv, "hvs:E:b:t:")) != -1)
+    {
+        switch (arg)
+        {
+        case 'h':
+            printUsage = 1;
+            printHelp();
+            //printf("Print usage info: %d\n", printUsage);
+            return 0;
+            break;
+        case 'v':
+            printTrace = 1;
+            //printf("Display trace info: %d\n", printTrace);
+            break;
+        case 's':
+            s = atoi(optarg);
+            S = pow(2, s);
+            //printf("s: %d, S: %d\n", s, S);
+            break;
+        case 'E':
+            E = atoi(optarg);
+            //printf("E: %d\n", E);
+            break;
+        case 'b':
+            b = atoi(optarg);
+            B = pow(2, b);
+            //printf("b: %d, B: %d\n", b, B);
+            break;
+        case 't':
+            strcpy(tracefile, optarg);
+            //printf("Name of valgrind trace to replay: %s\n", tracefile);
+            break;
+        default:
+            break;
+        }
+    }
+
+    int t = digits - s - b;
+
+    Cache* cache = InitCache(S, E, B);
+
+    FILE *file;
+    file = fopen(tracefile, "r");
+    if(!file) return -1;
+
+    char op;
+    unsigned int addr;
+    unsigned int size;
+
+    while (fscanf(file, " %c %x,%d", &op, &addr, &size) > 0)
+    {
+        unsigned CT = addr >> (s + b);
+        unsigned CI = (addr << t) >> (digits - s);
+        unsigned CO = (addr << (t + s)) >> (digits - b);
+        switch (op)
+        {
+        case 'L':
+            if (printTrace) printf("L %x,1", addr);
+            UpdateCache(cache, CT, CI, CO);
+            break;
+        case 'M':
+            if (printTrace) printf("M %x,1", addr);
+            UpdateCache(cache, CT, CI, CO);
+            UpdateCache(cache, CT, CI, CO);
+            break;
+        case 'S':
+            if (printTrace) printf("S %x,1", addr);
+            UpdateCache(cache, CT, CI, CO);
+            break;
+        default:
+            break;
+        }
+        if (printTrace) printf("\n");
+    }
+
+    printSummary(numHit, numMiss, numEvict);
+
+    DestroyCache(cache);
+    fclose(file);
+
     return 0;
 }
--- cachelab/cachelab-handout/trans.c
+++ cachelab/cachelab-handout/trans.c
@@ -12,6 +12,80 @@
 
 int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 
+void transpose_32x32(int M, int N, int A[N][M], int B[M][N])
+{
+    int tmp[8];
+    for (int i = 0; i < 32; i += 8)
+    {
+        for (int j = 0; j < 32; j += 8)
+        {
+            for (int a = i; a < i + 8; a++)
+            {
+                for (int m = 0; m < 8; m++) tmp[m] = A[a][j + m];
+                for (int m = 0; m < 8; m++) B[j + m][a] = tmp[m];
+
+            }
+        }
+    }
+}
+
+void transpose_64x64(int M, int N, int A[N][M], int B[M][N])
+{
+    int tmp[8];
+    for (int i = 0; i < 64; i += 8)
+    {
+        for (int j = 0; j < 64; j += 8)
+        {
+            // 每个8x8的数组(subA, subB)再分成2x2个大小为4x4的数组，索引分别为0，1，2，3
+
+            // subA          subB
+            // [0, 1         [0, 1
+            //  2, 3]        2, 3]
+
+            for (int a = i; a < i + 4; a++)
+            {
+                // 起始[a][j]
+                // 将转置后的subA的0、1号保存到subB的0、1号
+                for (int m = 0; m < 8; m++) tmp[m] = A[a][j + m];
+                for (int m = 0; m < 4; m++) B[j + m][a] = tmp[m];
+                for (int m = 0; m < 4; m++) B[j + m][a + 4] = tmp[m + 4];
+            }
+            for (int a = j; a < j + 4; a++)
+            {
+                // 起始[i][a]
+                // 保存subB的1号
+                tmp[0] = B[a][i + 4];
+                tmp[1] = B[a][i + 5];
+                tmp[2] = B[a][i + 6];
+                tmp[3] = B[a][i + 7];
+
+                // 保存subA的2号
+                tmp[4] = A[i + 4][a];
+                tmp[5] = A[i + 5][a];
+                tmp[6] = A[i + 6][a];
+                tmp[7] = A[i + 7][a];
+
+                // subA的2号转置到subB的3号
+                B[a][i + 4] = tmp[4];
+                B[a][i + 5] = tmp[5];
+                B[a][i + 6] = tmp[6];
+                B[a][i + 7] = tmp[7];
+
+                // subB的1号保存（平移）到subB的2号
+                B[a + 4][i + 0] = tmp[0];
+                B[a + 4][i + 1] = tmp[1];
+                B[a + 4][i + 2] = tmp[2];
+                B[a + 4][i + 3] = tmp[3];
+            }
+            for (int a = i + 4; a < i + 8; a++)
+            {
+                for (int m = 0; m < 4; m++) tmp[m] = A[a][j + 4 + m];
+                for (int m = 0; m < 4; m++) B[j + 4 + m][a] = tmp[m];                
+            }
+        }
+    }
+}
+
 /* 
  * transpose_submit - This is the solution transpose function that you
  *     will be graded on for Part B of the assignment. Do not change
@@ -22,6 +96,30 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 {
+    if (M == N && M == 32)
+    {
+        transpose_32x32(M, N, A, B);
+    }
+    else if (M == N && M == 64)
+    {
+        transpose_64x64(M, N, A, B);
+    }
+    else
+    {
+        for (int i = 0; i < N; i += 16)
+        {
+            for (int j = 0; j < M; j += 16)
+            {
+                for (int a = i; a < i + 16 && a < N; a++)
+                {
+                    for (int b = j; b < j + 16 && b < M; b++)
+                    {
+                        B[b][a] = A[a][b];
+                    }
+                }
+            }
+        }
+    }
 }
 
 /* 
